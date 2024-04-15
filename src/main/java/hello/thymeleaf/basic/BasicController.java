42ef837be0b0bfa7cf1e16f00276a079ab9acd4d
정재욱 <wjdwodnr5452@naver.com>
2024-04-15 23:16:57 +0900

    연산자

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -91,6 +91,19 @@ public class BasicController {
         return "basic/literal";
     }
 
+    @GetMapping("/operation")
+    public String operation(Model model){
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
+
+    @GetMapping("/attribute")
+    public String attribute(){
+        return "basic/attribute";
+    }
+
+
     @Data
     static class User{
         private String username;
